70122f940bfc8581f123703be9e1293688134d3f
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-10-13 00:54:23 +0530

    Functions for basic calculation of two numbers
    Addiitong, Subtraction, Multiplication and Division function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// CALCULATING FUNCTIONS
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
